8d47228b323ec95665541f95507572cf6f6cdcd2
Talgat <saparov.tn@phystech.edu>
2021-03-31 20:35:16 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,8 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-# automatically detect the lib name
-dirs = {d.parent.name for d in Path(__file__).resolve().parent.glob('*/__init__.py') if d.parent.is_dir()}
-dirs -= {'configs', 'notebooks', 'scripts', 'tests'}
-dirs = [n for n in dirs if not n.startswith(('_', '.'))]
-dirs = [n for n in dirs if not n.endswith(('.egg-info',))]
 
-assert len(dirs) == 1, dirs
-name = dirs[0]
-if name == 'my-project-name':
-    raise ValueError('Rename "my-project-name" to your project\'s name.')
+name = 'FBPAug'
 
 with open('requirements.txt', encoding='utf-8') as file:
     requirements = file.read().splitlines()
